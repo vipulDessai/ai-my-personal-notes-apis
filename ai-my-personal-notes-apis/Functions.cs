7f5ffd041a1094b40ecba78856719119bc45d5a5
Vipul Dessai <badgamerbad@gmail.com>
2023-11-27 02:21:21 +0530

    init

--- ai-my-personal-notes-apis/Functions.cs
+++ ai-my-personal-notes-apis/Functions.cs
@@ -2,6 +2,9 @@ using Amazon.Lambda.Annotations;
 using Amazon.Lambda.Annotations.APIGateway;
 using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
 using System.Net;
 using System.Text.Json;
 
@@ -9,6 +12,33 @@ using System.Text.Json;
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
 namespace ai_my_personal_notes_apis;
+
+//[BsonElement("PopulationAsOn")]
+//public DateTime? PopulationAsOn { get; set; }
+
+//[BsonRepresentation(BsonType.String)]
+//[BsonElement("CountryId")]
+//public int CountryId { get; set; }
+
+//[Required(AllowEmptyStrings = false)]
+//[StringLength(5)]
+//[BsonIgnoreIfDefault]
+//public virtual string CountryCode { get; set; }
+
+//[BsonIgnoreIfDefault]
+//public IList<States> States { get; set; }
+
+//[BsonExtraElements]
+//public BsonDocument ExtraElements { get; set; }
+class StoredSingleDbRecord
+{
+    [BsonId]
+    public string id { get; set; }
+
+    [BsonRepresentation(BsonType.String)]
+    [BsonElement("plot")]
+    public int plot { get; set; }
+}
 
 public class Functions
 {
@@ -19,22 +49,27 @@ public class Functions
     {
     }
 
+    private static MongoClient CreateMongoClient()
+    {
+        var mongoClientSettings = MongoClientSettings.FromConnectionString($"{Environment.GetEnvironmentVariable("MONGODB_URI")}");
+        mongoClientSettings.ServerApi = new ServerApi(ServerApiVersion.V1, strict: true);
+        return new MongoClient(mongoClientSettings);
+    }
+
 
     /// <summary>
     /// A Lambda function to respond to HTTP Get methods from API Gateway
     /// </summary>
     /// <remarks>
-    /// This uses the <see href="https://github.com/aws/aws-lambda-dotnet/blob/master/Libraries/src/Amazon.Lambda.Annotations/README.md">Lambda Annotations</see> 
-    /// programming model to bridge the gap between the Lambda programming model and a more idiomatic .NET model.
-    /// 
-    /// This automatically handles reading parameters from an APIGatewayProxyRequest
-    /// as well as syncing the function definitions to serverless.template each time you build.
-    /// 
-    /// If you do not wish to use this model and need to manipulate the API Gateway 
-    /// objects directly, see the accompanying Readme.md for instructions.
+    /// Store the form data in the mongo db
     /// </remarks>
+    /// <params>
+    /// <param name="request">request data from the client</param>
     /// <param name="context">Information about the invocation, function, and execution environment</param>
-    /// <returns>The response as an implicit <see cref="APIGatewayProxyResponse"/></returns>
+    /// <params>
+    /// <returns>
+    /// status of the data stored in the DB
+    /// </returns>
     [LambdaFunction(Policies = "AWSLambdaBasicExecutionRole", MemorySize = 256, Timeout = 30)]
     [RestApi(LambdaHttpMethod.Get, "/")]
     public APIGatewayProxyResponse Get(APIGatewayProxyRequest request, ILambdaContext context)
@@ -42,6 +77,12 @@ public class Functions
         context.Logger.LogInformation("Get Request\n");
 
         string reqString = JsonSerializer.Serialize(request);
+
+        var client = CreateMongoClient();
+        var collection = client.GetDatabase("sample_mflix").GetCollection<BsonDocument>("movies");
+        var filter = Builders<BsonDocument>.Filter.Eq("title", "Back to the Future");
+        var document = collection.Find(filter, new FindOptions() { BatchSize = 1000 }).First();
+        Console.WriteLine(document);
 
         var response = new APIGatewayProxyResponse
         {
