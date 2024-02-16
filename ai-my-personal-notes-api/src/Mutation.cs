79b5dfbc3d3b43720060ada726b7b4e014aaae9f
Vipul Dessai <badgamerbad@gmail.com>
2024-02-17 01:03:10 +0530

    code cleanup

--- ai-my-personal-notes-api/src/Mutation.cs
+++ ai-my-personal-notes-api/src/Mutation.cs
@@ -22,23 +22,6 @@ public class Mutation
     }
 
     [Authorize]
-    public async Task<AuthorPayload> AddAuthor(AuthorInput input, [Service] Repository repository)
-    {
-        var author = new Author(Guid.NewGuid(), input.name);
-        await repository.AddAuthor(author);
-        return new AuthorPayload(author);
-    }
-
-    public async Task<BookPayload> AddBook(BookInput input, [Service] Repository repository)
-    {
-        var author =
-            await repository.GetAuthor(input.author) ?? throw new Exception("Author not found");
-        var book = new Book(Guid.NewGuid(), input.title, author);
-        await repository.AddBook(book);
-        return new BookPayload(book);
-    }
-
-    [Authorize]
     public async Task<APIGatewayProxyResponse> AddNote(
         [GlobalState("currentUser")] CurrentUser user,
         AddNotesReqInput input
--- ai-my-personal-notes-api/src/Query.cs
+++ ai-my-personal-notes-api/src/Query.cs
@@ -1,52 +1,16 @@
-﻿using ai_my_personal_notes_api.Common;
+﻿using System.Net;
+using System.Text.Json;
+using ai_my_personal_notes_api.Common;
 using ai_my_personal_notes_api.Models;
 using ai_my_personal_notes_api.services;
 using Amazon.Lambda.APIGatewayEvents;
 using HotChocolate.Authorization;
 using MongoDB.Driver;
-using System.Net;
-using System.Text.Json;
 
 namespace ai_my_personal_notes_api;
 
 public class Query
 {
-    public async Task<APIGatewayProxyResponse> GetRestuarants(
-        [GlobalState("currentUser")] CurrentUser user,
-        GetRestuarantsInput input
-    )
-    {
-        var dbServer = new MongoDbServer();
-        var db = dbServer.client.GetDatabase("sample_restaurants");
-        var globalCollection = db.GetCollection<NoteSchema>("collection");
-        var _restaurantsCollection = db.GetCollection<Restaurant>("restaurants");
-        var filter = Builders<Restaurant>.Filter.Eq(input.FilterKey, input.FilterValue);
-        var findOptions = new FindOptions { BatchSize = input.BatchSize };
-
-        var res = new List<string>();
-        using (var cursor = _restaurantsCollection.Find(filter, findOptions).ToCursor())
-        {
-            if (cursor.MoveNext())
-            {
-                var d = cursor.Current.ToList();
-
-                for (int i = 0; i < d.Count; i++)
-                {
-                    res.Add(d[i].Name);
-                }
-            }
-        }
-
-        var response = new APIGatewayProxyResponse
-        {
-            StatusCode = (int)HttpStatusCode.OK,
-            Body = JsonSerializer.Serialize(res.ToArray()),
-            Headers = new Dictionary<string, string> { { "Content-Type", "text/plain" } }
-        };
-
-        return response;
-    }
-
     [Authorize]
     public async Task<APIGatewayProxyResponse> GetNotes(
         [GlobalState("currentUser")] CurrentUser user,
@@ -93,50 +57,6 @@ public class Query
 
         return response;
     }
-
-    public string Unauthorized()
-    {
-        return "unauthorized";
-    }
-
-    [Authorize(Policy = "hr")]
-    public List<string> Authorized([Service] IHttpContextAccessor contextAccessor)
-    {
-        var res = contextAccessor
-            .HttpContext.User.Claims.Select(x => $"{x.Type} : {x.Value}")
-            .ToList();
-
-        return res;
-    }
-
-    [Authorize(Policy = "hr")]
-    public List<string> AuthorizedBetterWay([GlobalState("currentUser")] CurrentUser user)
-    {
-        return user.Claims;
-    }
-
-    [Authorize(Roles = new[] { "leader" })]
-    public List<string> AuthorizedLeader([GlobalState("currentUser")] CurrentUser user)
-    {
-        return user.Claims;
-    }
-
-    [Authorize(Roles = new[] { "dev" })]
-    public List<string> AuthorizedDev([GlobalState("currentUser")] CurrentUser user)
-    {
-        return user.Claims;
-    }
-
-    [Authorize(Policy = "DevDepartment")]
-    public List<string> AuthorizedDevDepartment([GlobalState("currentUser")] CurrentUser user)
-    {
-        return user.Claims;
-    }
-
-    public Task<List<Book>> GetBooks([Service] Repository repository) => repository.GetBooksAsync();
-
-    public Task<Author?> GetAuthor(GetAuthorInput input, [Service] Repository r) =>
-        r.GetAuthor(input.authorId);
 
     public Task<APIGatewayProxyResponse> GetTags(GetTagsReqInput input)
     {
